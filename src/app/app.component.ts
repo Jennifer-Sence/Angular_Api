6ee6164a75d1350f8ddd8c0595bc97b001a4ca08
Jennifer-Sence <sencejenni@gmail.com>
2024-05-24 22:31:09 +0100

    searchId alterado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./master-page/header/header.component";
+import {FooterComponent} from "./master-page/footer/footer.component";
+import {NgFor, NgForOf} from "@angular/common";
+import {CardService} from "./components/card/card.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'api';
+
+  cards: string[]=[]
+
+  constructor(private cardService: CardService) {
+    console.log('constructor')
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.cards = this.cardService.getCards()
+  }
+
+
+  addCard() :void{
+    this.cards.push(String(this.cards.length + 1))
+}
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {MovieComponent} from "./components/movie/movie.component";
+import {MovieDetailsComponent} from "./pages/movie-details/movie-details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component: MovieComponent,
+  },
+  {
+    path: 'details',
+    component: MovieDetailsComponent,
+  }
+];
